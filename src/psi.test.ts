7de3942ee0561c93301c5c4de79432b02c4e3b9a
Wisdom Ogwu <iammadab@gmail.com>
2022-11-02 21:51:24 +0100

    implemented new psi interface

--- src/psi.test.ts
+++ src/psi.test.ts
@@ -7,38 +7,28 @@ describe('psi.js', () => {
   });
 
   it('blah', () => {
-    let psi_instance_1 = new PSI();
-    let psi_instance_2 = new PSI();
-
-    let set_a = [new Field(5), new Field(6), new Field(5)];
+    let set_a = [new Field(5), new Field(6), new Field(4)];
     let set_b = [new Field(4), new Field(5), new Field(7)];
 
-    console.log('set a');
-    print(set_a);
+    let psi_instance_1 = new PSI(set_a);
+    let psi_instance_2 = new PSI(set_b);
 
-    console.log('set b');
-    print(set_b);
+    let encrypted_a = psi_instance_1.getEncryptedSet();
 
-    // a will encrypt their values
-    let encrypted_set_a = psi_instance_1.encryptElements(set_a);
+    let shared_set_a = psi_instance_2.generateSharedSet(encrypted_a);
+    let encrypted_b = psi_instance_2.getEncryptedSet();
 
-    console.log('encrypted set a');
-    print(encrypted_set_a);
+    let index_values = psi_instance_1.processIntersectionGetIndex(
+      shared_set_a,
+      encrypted_b
+    );
+    let actual_values = psi_instance_1.processIntersectionGetValues(
+      shared_set_a,
+      encrypted_b
+    );
 
-    // b will encrypt their values and encrypt b's values
-    let encrypted_set_b = psi_instance_2.encryptElements(set_b);
-
-    console.log('encrypted set b');
-    print(encrypted_set_b);
-
-    let shared_set_a = psi_instance_2.encryptElements(encrypted_set_a);
-    let shared_set_b = psi_instance_1.encryptElements(encrypted_set_b);
-
-    console.log('shared set a');
-    print(shared_set_a);
-
-    console.log('shared set b');
-    print(shared_set_b);
+    console.log(index_values);
+    print(actual_values);
   });
 });
 
--- src/psi.ts
+++ src/psi.ts
@@ -3,18 +3,64 @@ import { exp } from './util';
 
 export default class PSI {
   private private_key: Field;
+  private unencrypted_set: Field[];
+  private encrypted_set: Field[];
 
-  constructor() {
+  constructor(set_values: Field[]) {
     this.private_key = Field.random();
+    this.unencrypted_set = set_values;
+    this.encrypted_set = this.encryptElements(this.unencrypted_set);
   }
 
-  getPrivateKey(): Field {
-    return this.private_key;
-  }
-
-  encryptElements(elements: Field[]): Field[] {
+  // encrypts the element of the provided set using the generated private key
+  private encryptElements(elements: Field[]): Field[] {
     return elements.map((element) => {
       return exp(element, this.private_key.toBigInt());
     });
+  }
+
+  getEncryptedSet(): Field[] {
+    return this.encrypted_set;
+  }
+
+  generateSharedSet(other_encrypted_set: Field[]): Field[] {
+    return this.encryptElements(other_encrypted_set);
+  }
+
+  processIntersectionGetIndex(
+    shared_set: Field[],
+    other_encrypted_set: Field[]
+  ): number[] {
+    const intersecting_index: number[] = [];
+    // TODO: possible rename from other??
+    const other_shared_set = this.encryptElements(other_encrypted_set);
+    // build hashmap from shared set to index
+    const shared_key_to_index_map: Map<string, number> = new Map();
+    shared_set.map((value, index) => {
+      console.log(value.toString());
+      shared_key_to_index_map.set(value.toString(), index);
+    });
+    console.log(shared_key_to_index_map);
+    // iterate over other shared set, checking for intersection
+    other_shared_set.map((value) => {
+      console.log(value.toString());
+      let index = shared_key_to_index_map.get(value.toString());
+      console.log(index);
+      if (index != undefined) {
+        intersecting_index.push(index);
+      }
+    });
+    return intersecting_index;
+  }
+
+  processIntersectionGetValues(
+    shared_set: Field[],
+    other_encrypted_set: Field[]
+  ): Field[] {
+    const index_values = this.processIntersectionGetIndex(
+      shared_set,
+      other_encrypted_set
+    );
+    return index_values.map((index) => this.unencrypted_set[index]);
   }
 }
