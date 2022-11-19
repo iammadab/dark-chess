024dadf6e3d07775ef72fdb45fc3c3e5514bc7ba
Wisdom Ogwu <iammadab@gmail.com>
2022-11-19 20:40:41 +0100

    serving basic html page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,4 +12,5 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'snarkyjs'],
   rules: {},
+  ignorePatterns: ['*.html'],
 };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+import path from 'path';
 import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.sendFile(path.resolve('./server/public/index.html'));
 });
+
+app.use('/zkapp', express.static(path.resolve('./zkapp/build/zkapp/src')));
 
 app.listen(3000, () => {
   console.log('Application listening at port 3000');
